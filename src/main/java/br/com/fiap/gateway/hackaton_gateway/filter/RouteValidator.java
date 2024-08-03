2422b3459709f1f2b90393298a0445ca492f8598
devops.jeffersantos <devops.jeffersantos@gmail.com>
2024-08-02 23:56:09 -0300

    change for swagger

--- src/main/java/br/com/fiap/gateway/hackaton_gateway/filter/RouteValidator.java
+++ src/main/java/br/com/fiap/gateway/hackaton_gateway/filter/RouteValidator.java
@@ -10,11 +10,22 @@ import java.util.function.Predicate;
 public class RouteValidator {
 
     public static final List<String> openApiEndpoints = List.of(
-            "/api/autenticacao/registrar",
-            "/api/autenticacao/todos",
             "/api/autenticacao/autenticar",
+            "/hackaton-clientes-app/v3/api-docs",
+            "/hackaton-cartao-app/v3/api-docs",
+            "/hackaton-pagamentos-app/v3/api-docs",
+            "/hackaton-autenticacao-app/v3/api-docs",
             "/eureka",
-            "/swagger-ui/**"
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/v3/api-docs.yaml",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**"
     );
 
     public Predicate<ServerHttpRequest> isSecured =
