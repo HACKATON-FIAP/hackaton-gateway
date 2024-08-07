dc1a0c13f5177add600b90d2fd7f7cb83d7314b8
devops.jeffersantos <devops.jeffersantos@gmail.com>
2024-08-06 23:57:10 -0300

    ok

--- src/main/java/br/com/fiap/gateway/hackaton_gateway/filter/RouteValidator.java
+++ src/main/java/br/com/fiap/gateway/hackaton_gateway/filter/RouteValidator.java
@@ -11,21 +11,21 @@ public class RouteValidator {
 
     public static final List<String> openApiEndpoints = List.of(
             "/api/autenticacao/autenticar",
-            "/hackaton-clientes-app/v3/api-docs",
-            "/hackaton-cartao-app/v3/api-docs",
-            "/hackaton-pagamentos-app/v3/api-docs",
-            "/hackaton-autenticacao-app/v3/api-docs",
-            "/eureka",
-            "/swagger-ui.html",
-            "/swagger-ui/**",
-            "/v3/api-docs/**",
-            "/v3/api-docs.yaml",
-            "/swagger-resources",
-            "/swagger-resources/**",
-            "/configuration/ui",
-            "/configuration/security",
-            "/swagger-ui/**",
-            "/webjars/**"
+//            "/hackaton-clientes-app/v3/api-docs",
+//            "/hackaton-cartao-app/v3/api-docs",
+//            "/hackaton-pagamentos-app/v3/api-docs",
+//            "/hackaton-autenticacao-app/v3/api-docs",
+            "/eureka"
+//            "/swagger-ui.html",
+//            "/swagger-ui/**",
+//            "/v3/api-docs/**",
+//            "/v3/api-docs.yaml",
+//            "/swagger-resources",
+//            "/swagger-resources/**",
+//            "/configuration/ui",
+//            "/configuration/security",
+//            "/swagger-ui/**",
+//            "/webjars/**"
     );
 
     public Predicate<ServerHttpRequest> isSecured =
